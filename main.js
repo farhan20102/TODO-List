70ed3335b81966368e52f664701c38db5e5db6ec
farhan20102 <142581932+farhan20102@users.noreply.github.com>
2023-08-25 21:04:52 +1000

    main.js

--- main.js
+++ main.js
@@ -1,72 +1,76 @@
 const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-function displayTask(){
-    const taskList = document.getElementById('task-list');
-    taskList.innerHTML = '';
 
-    savedTasks.forEach((task, index) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = task;
-      listItem.addEventListener('click', () => {
-        listItem.classList.toggle('completed');
-        updateLocalStorage();
-      });
+function displayTasks() {
+  const taskList = document.getElementById('task-list');
+  taskList.innerHTML = '';
 
-      const deleteButton = document.createElement('button');
-      deleteButton.classList.add('deleteBtn');
-      deleteButton.innerHTML = '<i class="fa-solid fa-delete-left"></i>';
-      deleteButton.addEventListener('click', () => {
-        savedTasks.splice(index,1);
-        displayTask();
-        updateLocalStorage();
-      });
-      
-      const editButton = document.createElement('button');
-      editButton.classList.add('editBtn');
-      editButton.innerHTML = '<i class = "fa-solid fa-pencil"></i>'
-      editButton.addEventListener('click', () => {
-        const editInput = createElementInput;
-        editInput.classList.add('editInp');
-        editInput.type = 'text';
-        editInput.value = task;
-        editInput.addEventListener('keypress', (event) => {
-          if(event.keyCode===13){
-            const newTask = editInput.value.trim();
-            if(newTask!==''){
-              savedTasks[index] = newTask;
-              displayTask();
-              updateLocalStorage();
-            }
-          }
-        });
-        listItem.textContent = '';
-        listItem.appendChild(editInput);
-        editInput.focus();
-      });
-      listItem.appendChild(deleteButton);
-      listItem.appendChild(editButton);
-      taskList.appendChild(listItem);
+  savedTasks.forEach((task, index) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = task;
+    listItem.addEventListener('click', () => {
+      listItem.classList.toggle('completed');
+      updateLocalStorage();
     });
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('deletebtn');
+    deleteButton.innerHTML = '<i class="fa-solid fa-delete-left"></i>';
+    deleteButton.addEventListener('click', () => {
+      savedTasks.splice(index, 1);
+      displayTasks();
+      updateLocalStorage();
+    });
+
+    const editButton = document.createElement('button');
+    editButton.classList.add('editbtn');
+    editButton.innerHTML = '<i class="fa-solid fa-pencil"></i>';
+    editButton.addEventListener('click', () => {
+      const editInput = document.createElement('input');
+      editInput.classList.add('editinput')
+      editInput.type = 'text';
+      editInput.value = task;
+      editInput.addEventListener('keypress', (event) => {
+        if (event.keyCode === 13) {
+          const newTask = editInput.value.trim();
+          if (newTask !== '') {
+            savedTasks[index] = newTask;
+            displayTasks();
+            updateLocalStorage();
+          }
+        }
+      });
+
+      listItem.textContent = '';
+      listItem.appendChild(editInput);
+      editInput.focus();
+    });
+
+    listItem.appendChild(deleteButton);
+    listItem.appendChild(editButton);
+    taskList.appendChild(listItem);
+  });
 }
 
-function addTask(){
-    const taskInput = document.getElementById('taskInput');
-    const task = taskInput.value.trim();
-    if(task !== ''){
-        savedTasks.push(task);
-        displayTask();
-        updateLocalStorage(); 
-        taskInput.value = '';
-    }
+function addTask() {
+  const taskInput = document.getElementById('task-input');
+  const task = taskInput.value.trim();
+
+  if (task !== '') {
+    savedTasks.push(task);
+    displayTasks();
+    updateLocalStorage();
+    taskInput.value = '';
+  }
 }
 
 function updateLocalStorage() {
-    localStorage.setItem('tasks', JSON.stringify(savedTasks));
-  }
-  
-document.getElementById('addButton').addEventListener('click', addTask);
+  localStorage.setItem('tasks', JSON.stringify(savedTasks));
+}
 
-displayTask();
-  
+document.getElementById('add-button').addEventListener('click', addTask);
+
+displayTasks();
+
 const taskInput = document.getElementById('taskInput');
 taskInput.addEventListener('keypress', function(event) {
   if (event.keyCode === 13) {
